837eb146aeae5ef9ebbcbcc157aa9f8e5a41b197
doge <gateno103@gmail.com>
2022-12-17 16:15:47 +0200

    Changes to be committed: 	renamed:    twitchbot.py -> bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,20 @@ from twitchio.ext import commands
 
 bot = commands.Bot(
     # set up the bot
-    irc_token=os.environ['TMI_TOKEN'],
+    token=os.environ['TMI_TOKEN'],
     client_id=os.environ['CLIENT_ID'],
     nick=os.environ['BOT_NICK'],
     prefix=os.environ['BOT_PREFIX'],
     initial_channels=[os.environ['CHANNEL']]
 )
+
+
+@bot.event
+async def event_ready():
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws
+    await ws.send_privms(os.environ['CHANNEL'], f'/me has landed')
+
 # bot.py
 if __name__ == "__main__":
     bot.run()
